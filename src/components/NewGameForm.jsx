3501f1de1838931d6751499756abc8e9adcc5aad
Thiago <thiago_99_oliveira@hotmail.com>
2024-03-25 22:33:33 -0300

    criando componentes e hooks

---
+++ src/components/NewGameForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import PropTypes from 'prop-types'
+import TextInput from "./TextInput"
+
+NewGameForm.propTypes = {
+    addGame: PropTypes.func
+}
+
+export default function NewGameForm ({addGame}) { 
+    const [title, setTitle] = useState("")
+    const [cover, setCover] = useState("")
+    
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        addGame({title, cover})
+        setTitle("")
+        setCover("")
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextInput id="title" label="Título:" value={title} setValue={setTitle}/>
+            <TextInput id="cover" label="Capa:" value={cover} setValue={setCover}/>
+        <button type="submit">Adicionar a biblioteca</button>
+      </form>
+    )
+}
